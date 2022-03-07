9f2738d8ce73137060564cb84c6baa566726d79b
hhj <sbs98010@naver.com>
2022-03-08 04:36:19 +0900

    7-1

---
+++ part7/src/main/java/com/example/part7/Lab7_1.kt
@@ -0,0 +1,11 @@
+package com.example.part7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lab7_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab7_1)
+    }
+}
