e556fda12042f21add82ac4c92410cc18fe774a5
hhj <sbs98010@naver.com>
2022-03-12 22:31:36 +0900

    ch8

--- part8/src/main/java/com/example/part8/Ch8_Event.kt
+++ part8/src/main/java/com/example/part8/Ch8_Event.kt
@@ -2,10 +2,16 @@ package com.example.part8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.part8.databinding.ActivityCh8EventBinding
 
 class Ch8_Event : AppCompatActivity() {
+    var initTime=0L
+    var pauseTime=0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ch8_event)
+
+        val binding=ActivityCh8EventBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
