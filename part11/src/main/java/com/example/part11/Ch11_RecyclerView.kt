adb239a6333c3fb5103b647ab318e25f43e7a66f
hhj <sbs98010@naver.com>
2022-05-23 01:40:54 +0900

    part11

---
+++ part11/src/main/java/com/example/part11/Ch11_RecyclerView.kt
@@ -0,0 +1,11 @@
+package com.example.part11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ch11_RecyclerView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ch11_recycler_view)
+    }
+}
