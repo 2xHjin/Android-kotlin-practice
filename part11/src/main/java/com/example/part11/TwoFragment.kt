07abd3db31a37e2e9c2f9e8db94b089e7e3d5492
hhj <sbs98010@naver.com>
2022-05-27 00:49:25 +0900

    part11

---
+++ part11/src/main/java/com/example/part11/TwoFragment.kt
@@ -0,0 +1,20 @@
+package com.example.part11
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ch11_jetpack.databinding.FragmentTwoBinding
+import com.example.part11.databinding.FragmentTwoBinding
+
+class TwoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentTwoBinding.inflate(inflater, container, false).root
+    }
+
+}
