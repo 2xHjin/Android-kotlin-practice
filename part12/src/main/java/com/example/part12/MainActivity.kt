0923dadf27341c3c65e21baf3f8278aebfce58e5
hhj <sbs98010@naver.com>
2022-06-06 20:35:33 +0900

    12ing

--- part12/src/main/java/com/example/part12/MainActivity.kt
+++ part12/src/main/java/com/example/part12/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.part12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
