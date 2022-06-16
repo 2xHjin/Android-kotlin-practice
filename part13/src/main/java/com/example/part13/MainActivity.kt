8c9eebb26f3506347f3f05ff8d452bcde5cf81b7
hhj <sbs98010@naver.com>
2022-06-16 18:21:16 +0900

    13 M+A

--- part13/src/main/java/com/example/part13/MainActivity.kt
+++ part13/src/main/java/com/example/part13/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.part13
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.part13.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    var datas:MutableList<String>?=null
+    lateinit var adapter:MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
