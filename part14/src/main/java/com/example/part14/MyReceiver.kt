c6545d08f728d5042d50d5acd55cdc013982d8af
hhj <sbs98010@naver.com>
2022-06-22 18:47:49 +0900

    ...

--- part14/src/main/java/com/example/part14/MyReceiver.kt
+++ part14/src/main/java/com/example/part14/MyReceiver.kt
@@ -13,44 +13,51 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 
-class MyReceiver: BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
+class MyReceiver : BroadcastReceiver() {
 
-        Log.d("kkang","MyReceiver....")
-        val manager=context.getSystemService(AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d("kkang","MyReceiver...............")
+        val manager = context.getSystemService(AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
         val builder: NotificationCompat.Builder
 
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-            val channelId="one-channel"
-            val channelName="My Channel One"
-            val channel= NotificationChannel(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //26버전 이상
+            val channelId = "one-channel"
+            val channelName = "My Channel One"
+            val channel = NotificationChannel(
                 channelId,
                 channelName,
                 NotificationManager.IMPORTANCE_DEFAULT
-            ).apply{
-                description="My Channel One Description"
+            ).apply {
+                //채널에 다양한 정보 설정
+                description = "My Channel One Description"
                 setShowBadge(true)
                 val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-                val audioAttributes= AudioAttributes.Builder()
+                val audioAttributes = AudioAttributes.Builder()
                     .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
                     .setUsage(AudioAttributes.USAGE_ALARM)
                     .build()
-                setSound(uri,audioAttributes)
+                setSound(uri, audioAttributes)
                 enableVibration(true)
             }
+
+            //채널을 NotificationManager에 등록
             manager.createNotificationChannel(channel)
 
-            builder=NotificationCompat.Builder(context,channelId)
+            //채널을 이용하여 builder 생성
+            builder = NotificationCompat.Builder(context, channelId)
+        } else {
+            //26버전 이하
+            builder = NotificationCompat.Builder(context)
         }
-        else{
-            builder=NotificationCompat.Builder(context)
-        }
-        builder.run{
+
+        builder.run {
+            //알림의 기본 정보
             setSmallIcon(android.R.drawable.ic_notification_overlay)
             setWhen(System.currentTimeMillis())
             setContentTitle("홍길동")
             setContentText("안녕하세요")
         }
-        manager.notify(11,builder.build())
+        manager.notify(11, builder.build())
     }
 }
