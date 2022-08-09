e5a3be6083d8eab4813c3c47cbfb393f2b33b5f3
hhj <sbs98010@naver.com>
2022-08-09 23:32:22 +0900

    16ed

--- part16/src/main/java/com/example/part16/MainActivity.kt
+++ part16/src/main/java/com/example/part16/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.example.part16.databinding.ActivityMainBinding
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -79,7 +83,23 @@ class MainActivity : AppCompatActivity() {
         binding.cameraButton.setOnClickListener {
             //camera app......................
             //파일 준비...............
-            
+            val timeStamp:String=
+                SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            val storageDir:File?=
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            val file=File.createTempFile(
+                "JPEG_${timeStamp}_",
+                ".jpg",
+                storageDir
+            )
+            filePath=file.absolutePath
+            val photoURI:Uri=FileProvider.getUriForFile(
+                this,
+                "com.example.part16.fileprovider",file
+            )
+            val intent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
+            requestCameraFileLauncher.launch(intent);
         }
     }
 
