88194545a69219a9d99c1d1936eff0936ad5e431
hhj <sbs98010@naver.com>
2022-08-15 03:04:06 +0900

    17 15를해보자

--- part17/src/main/java/com/example/part17/MainActivity.kt
+++ part17/src/main/java/com/example/part17/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.example.part17
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ch17_database.MyAdapter
+import com.example.part17.AddActivity
+import com.example.part17.R
+import com.example.part17.SettingActivity
+import com.example.part17.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var datas: MutableList<String>? = null
+    lateinit var adapter: MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val requestLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult())
+        {
+            it.data!!.getStringExtra("result")?.let {
+                datas?.add(it)
+                adapter.notifyDataSetChanged()
+            }
+        }
+        binding.mainFab.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            requestLauncher.launch(intent)
+        }
+
+        datas= mutableListOf<String>()
+
+        //add......................
+
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.mainRecyclerView.layoutManager=layoutManager
+        adapter=MyAdapter(datas)
+        binding.mainRecyclerView.adapter=adapter
+        binding.mainRecyclerView.addItemDecoration(
+            DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
+        )
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId===R.id.menu_main_setting){
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
