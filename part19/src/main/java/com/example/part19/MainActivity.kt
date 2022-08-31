860a2ee50fe222b6f73daffe92ef381665d4e3f2
hhj <sbs98010@naver.com>
2022-08-31 23:58:46 +0900

    api키

--- part19/src/main/java/com/example/part19/MainActivity.kt
+++ part19/src/main/java/com/example/part19/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.part19
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , GoogleApiClient.ConnectionCallbacks,
+GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
+    lateinit var providerClient: FusedLocationProviderClient
+    lateinit var apiClient:GoogleApiClient
+    var googleMap: GoogleMap?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var requestPermissionLauncher=registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){
+        if(it.all { permission->permission.value==true}){
+            apiClient.connect()
+        }else{
+            Toast.makeText(this,"권한거부",Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
